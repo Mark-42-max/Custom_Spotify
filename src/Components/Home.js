f4fd95329e82739dc399c1d68952d6712fdae494
Arijit-101 <arijitsaha3000@gmail.com>
2022-04-15 01:34:36 +0530

    commit1

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>Home
+      <div className="name">
+        Hello byyeeDFsFzsdsdafjkfj
+      </div>
+    </div>
+    
+
+
+
   )
 }
 
